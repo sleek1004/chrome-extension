e81dc0d660c423eee7ac71e0bb16d9fe90df98d2
sleek1004 <arojojoyeo@yahoo.com>
2022-09-21 18:05:41 +0100

    bulding chrome extension with html,css and JavaScript

---
+++ index.js
@@ -0,0 +1,10 @@
+function input(){
+    console.log("clicked")
+}
+
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function(){
+    console.log("Button clicked from addEventListener")
+})
+ 
