fe19168d8347c85ff186f3dd34a83efc7ff9925a
sleek1004 <arojojoyeo@yahoo.com>
2022-09-24 20:54:59 +0100

    chrome extension int the making

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-let myLeads = []
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
  
 const inputBtn = document.getElementById("input-btn")
 const inputEL = document.getElementById("input-el")
+const ulEL = document.getElementById("ul-el")
+
+
 
 inputBtn.addEventListener("click", function(){
-   myLeads.push("www.awesomelead.com")
+   myLeads.push(inputEL.value)
    console.log(myLeads)
 })
+
+
+
+for(let i = 0; i< myLeads.length; i ++){
+   ulEL.innerHTML += "<li>"  + myLeads[i] + "</li> "
+}
   
+
