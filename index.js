c5246b2b3c01683b50b0512b914e9e6cb209dbd2
sleek1004 <arojojoyeo@yahoo.com>
2022-09-30 23:36:30 +0100

    chrome extension built with HTML,CSS AND JavaScript

--- index.js
+++ index.js
@@ -1,53 +1,66 @@
 let myLeads = []
- 
+
+const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
-const inputEL = document.getElementById("input-el")
-const ulEL = document.getElementById("ul-el")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const saveBtn = document.getElementById("save-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
 
 
+}
+const tabs = [
+   {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
 
-//let myName = localStorage.getItem("myName")
-//console.log(myName)
-//localStorage.clear()
+saveBtn.addEventListener("click", function(){
+  chrome.tabs.query({active: true, currentWindow:true}, function (tab){
+   myLeads.push(tabs[0].url)
+   saveBtn.value=""
+   localStorage.setItem("myLeads", JSON.stringify(myLeads))
+   render(myLeads)
 
-
-
-
-
-
-inputBtn.addEventListener("click", function(){
-   myLeads.push(inputEL.value)
-   inputEL.value = " "
+  })
    
-   localStorage.setItem("myLeads", JSON.stringify(myLeads)) 
-
+ 
+   })
 
    
-   renderLeads()
-   console.log(localStorage.getItem("myLeads"))
+   
+
+
+
+function render(Leads) {
+    let listItems = ""
+    for (let i = 0; i < Leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${Leads[i]}'>
+                    ${Leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
 })
 
 
-function renderLeads(){
 
-
-let listItems = " "
-
-for(let i = 0; i< myLeads.length; i ++){
-   //listItems += "<li><a target ='_blank' href=' " + myLeads[i] + "'>"  +  myLeads[i]     + "</a></li> "
-
-   //simpler and better way.. using template
-
-   listItems += `
-   <li>
-    <a target='_blank' href='${myLeads[i]}'>
-    ${myLeads[i]} </a>
-
-      </li>
-      `
-
-   ulEL.innerHTML = listItems
-
-}
-}
 
