db7d61eadba78fb48e55d7de3cb16ade46090a1a
sleek1004 <arojojoyeo@yahoo.com>
2022-10-02 23:05:12 +0100

    well built chrome extension

--- index.js
+++ index.js
@@ -1,46 +1,31 @@
 let myLeads = []
-
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-const saveBtn = document.getElementById("save-btn")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
-
-
 }
-const tabs = [
-   {url: "https://www.linkedin.com/in/per-harald-borgen/"}
-]
 
-saveBtn.addEventListener("click", function(){
-  chrome.tabs.query({active: true, currentWindow:true}, function (tab){
-   myLeads.push(tabs[0].url)
-   saveBtn.value=""
-   localStorage.setItem("myLeads", JSON.stringify(myLeads))
-   render(myLeads)
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
 
-  })
-   
- 
-   })
-
-   
-   
-
-
-
-function render(Leads) {
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < Leads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${Leads[i]}'>
-                    ${Leads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `
@@ -60,7 +45,3 @@ inputBtn.addEventListener("click", function() {
     localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     render(myLeads)
 })
-
-
-
-
